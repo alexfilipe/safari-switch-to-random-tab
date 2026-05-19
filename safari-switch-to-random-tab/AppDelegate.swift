6a0e23929936aabaa76069250b99637dba7b8789
Álex Santos <alexfilipecbs@gmail.com>
2026-05-19 04:52:00 -0700

    Initial Commit

---
+++ safari-switch-to-random-tab/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  safari-switch-to-random-tab
+//
+//  Created by Álex on 5/19/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
