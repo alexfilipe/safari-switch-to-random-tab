0f1a375cd00d3f0042c9bdc479a84db69d04ced7
Álex Santos <alexfilipecbs@gmail.com>
2026-05-19 05:04:52 -0700

    initial extension

--- safari-switch-to-random-tab Extension/Resources/background.js
+++ safari-switch-to-random-tab Extension/Resources/background.js
@@ -1,6 +1,12 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+browser.action.onClicked.addListener(async () => {
+    const tabs = await browser.tabs.query({ currentWindow: true });
+    const inactiveTabs = tabs.filter((tab) => !tab.active && tab.id !== undefined);
 
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
+    if (inactiveTabs.length === 0)
+        return;
+
+    const randomIndex = Math.floor(Math.random() * inactiveTabs.length);
+    const randomTab = inactiveTabs[randomIndex];
+
+    await browser.tabs.update(randomTab.id, { active: true });
 });
